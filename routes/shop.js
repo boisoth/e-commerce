a40d3fc697337e8fa0170aac7fbd85325f5417b8
Boi Soth <boi@cuey.io>
2019-11-20 15:08:34 -0800

    adding some dummy data on the server for /shop route from /add-product redirect

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ const router = express.Router();
 // @access Public
 
 router.get("/", (req, res, next) => {
+  console.log("shop.js", adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
